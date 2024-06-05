51419c4c8c6c50c46b50e3725515cc83a39e359d
Jules <testadmin@Julias-MBP-2.home>
2024-06-05 12:48:14 +0200

    adapt code so that test Conjured Mana Cake normal is green again

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -18,8 +18,10 @@ function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (items[i].name != 'Sulfuras, Hand of Ragnaros' && !items[i].name.includes('Conjured')) {
           items[i].quality = items[i].quality - 1
+        } else if (items[i].name.includes('Conjured')){
+          items[i].quality = items[i].quality - 2
         }
       }
     } else {
