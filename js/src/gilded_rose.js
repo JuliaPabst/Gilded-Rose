d5b2ac61d8f3722c6cbb7f06406ca37295f34585
Jules <testadmin@Julias-MBP-2.home>
2024-06-05 13:00:06 +0200

    refactor

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -16,15 +16,15 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 function update_quality() {
   console.log(items);
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && items[i].name != 'Sulfuras, Hand of Ragnaros') {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros' && !items[i].name.includes('Conjured')) {
-          items[i].quality = items[i].quality - 1
-        } else if (items[i].name.includes('Conjured')){
+        if (items[i].name.includes('Conjured')){
           items[i].quality = items[i].quality - 2
-        }
+        } else {
+          items[i].quality = items[i].quality - 1;
+        } 
       }
-    } else {
+    } else if (items[i].name != 'Sulfuras, Hand of Ragnaros'){
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -45,24 +45,18 @@ function update_quality() {
       items[i].sell_in = items[i].sell_in - 1;
     }
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
+      if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (items[i].quality > 0) {
+          items[i].quality = items[i].quality - 1
+        } else if (items[i].quality < 50){
+          items[i].quality = items[i].quality + 1
+        } else  {
           items[i].quality = items[i].quality - items[i].quality
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
+      } else if (items[i].name == 'Aged Brie' && items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
+        items[i].quality = 0;
       }
     }
-    console.log(items[i].sell_in);
-    console.log(items[i].quality);
   }
 
 }
