eb2c386b853c9cdafc11744e542ae0306d1333af
Jules <testadmin@Julias-MBP-2.home>
2024-06-05 12:26:13 +0200

    Aged Brie normal

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -24,4 +24,11 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(19);
   });
 
+  it("Aged Brie normal", function() {
+    items = [ new Item('Aged Brie', 2, 0) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(1);
+    expect(items[0].quality).toEqual(1);
+  });
+
 });
